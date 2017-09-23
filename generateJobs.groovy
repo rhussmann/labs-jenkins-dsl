5e63f851f5bc13e89a8a33e5e2d6b8b56fc996a5
Ricky Hussmann <ricky.hussmann@gmail.com>
2017-09-23 12:27:07 -0400

    Add beta.rickyandjodi.com job

--- generateJobs.groovy
+++ generateJobs.groovy
@@ -16,3 +16,22 @@ pipelineJob('lists.rhussmann.com') {
     colorizeOutput()
   }
 }
+
+pipelineJob('beta.rickyandjodi.com') {
+  definition {
+    cpsScm {
+      scm {
+	git {
+	  remote {
+	    credentials('ssh.beta.rickyandjodi.com')
+	    url('git@github.com:rhussmann/rickyandjodi.com.git')
+	  }
+	}
+      }
+      scriptPath('jenkins-hooks/Jenkinsfile')
+    }
+  }
+  wrappers {
+    colorizeOutput()
+  }
+}
